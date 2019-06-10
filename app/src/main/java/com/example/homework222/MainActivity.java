93adf563ae1fd9b34a13f29c05565d8a02502e4c
maximtosh <mr.maks.filippov.94@mail.ru>
2019-06-11 00:26:10 +0300

    assa

--- app/src/main/java/com/example/homework222/MainActivity.java
+++ app/src/main/java/com/example/homework222/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.homework222;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+//import android.widget.Toolbar;
+//import android.support.v7.widget.Toolbar;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -16,8 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intentNotes = new Intent(MainActivity.this, NotesActivity.class);
-        startActivity(intentNotes);
+
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(toolbar);
@@ -38,6 +40,29 @@ public class MainActivity extends AppCompatActivity {
 
         if (id == R.id.action_open_notes) {
             Toast.makeText(MainActivity.this, "Отркыть записную книжку", Toast.LENGTH_LONG).show();
+            Intent intentNotes = new Intent(MainActivity.this, NotesActivity.class);
+            startActivity(intentNotes);
+            return true;
+        }
+
+        if (id == R.id.mutually_exclusive_checkbox) {
+            Toast.makeText(MainActivity.this, "Отркыть взаимоисключающие checkbox", Toast.LENGTH_LONG).show();
+            Intent intentNotes = new Intent(MainActivity.this, CheckboxActivity.class);
+            startActivity(intentNotes);
+            return true;
+        }
+
+        if (id == R.id.spinner_country) {
+            Toast.makeText(MainActivity.this, "Отркыть Spinner «Страны-города-улицы»", Toast.LENGTH_LONG).show();
+            Intent intentNotes = new Intent(MainActivity.this, SpinnerActivity.class);
+            startActivity(intentNotes);
+            return true;
+        }
+
+        if (id == R.id.calendar) {
+            Toast.makeText(MainActivity.this, "Отркыть CalendarView «Сроки задачи»", Toast.LENGTH_LONG).show();
+            Intent intentNotes = new Intent(MainActivity.this, CalendarActivity.class);
+            startActivity(intentNotes);
             return true;
         }
 
